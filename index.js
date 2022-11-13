580dea045cc6bf8289efbd889c867ef9bed76f2f
Michal3333 <michalrajczak33@gmail.com>
2022-11-13 18:20:03 +0100

    add inquirer

--- index.js
+++ index.js
@@ -1,27 +1,63 @@
 #! /usr/bin/env node
+import inquirer from 'inquirer';
 import { logSuccess, logTitle } from './utils/logger.js';
-import runCommand, {handleError} from './utils/commandRunner.js';
+import runCommand, { handleError } from './utils/commandRunner.js';
+
+const completeValue = 'complete';
+const minimumValue = 'minimum';
 
 const repoUrl = 'https://github.com/Michal3333/rest-express-template';
-const projectDirectory = process.argv[2];
 
-if(!projectDirectory) {
-  handleError('The project directory was not specified.')
-}
+(async () => {
+  const answers = await inquirer.prompt([
+    {
+      name: 'directory',
+      message: 'Directory name:',
+      type: 'Input',
+    }, {
+      name: 'type',
+      message: 'Project type:',
+      type: 'list',
+      choices: [
+        {
+          name: 'Complete - copies the complete version of the template',
+          value: completeValue,
+        }, {
+          name: 'Minimum - copies the project configuration and creates a minimal express setup',
+          value: minimumValue,
+        },
+      ],
+    },
+  ]);
 
-if(projectDirectory.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-  handleError('The project name is invalid.');
-}
+  const projectDirectory = answers.directory;
+  const projctType = answers.type;
 
-const gitCloneCommand = `git clone --depth 1 ${repoUrl} ${projectDirectory}`;
-const deleteRemoteGitCommand = `cd ${projectDirectory} && rm -rf .git`;
-const installCommand = `cd ${projectDirectory} && npm install`;
-const createEnvCommand = `cd ${projectDirectory} && cp .env.sample .env`;
+  if (!projectDirectory) {
+    handleError('The project directory was not specified.');
+  }
 
+  if (projectDirectory.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+    handleError('The project name is invalid.');
+  }
 
-runCommand(gitCloneCommand, '[1/4] Repo cloned successfully!');
-runCommand(deleteRemoteGitCommand, '[2/4] .git folder removed successfully!');
-runCommand(installCommand, '[3/4] Dependencies installed successfully!');
-runCommand(createEnvCommand, '[4/4] .env file created successfully!');
+  const gitCloneCommand = `git clone --depth 1 ${repoUrl} ${projectDirectory}`;
+  const deleteRemoteGitCommand = `cd ${projectDirectory} && rm -rf .git`;
+  const installCommand = `cd ${projectDirectory} && npm install`;
+  const createEnvCommand = `cd ${projectDirectory} && cp .env.sample .env`;
+  const deleteStarterIndexCommand = `cd ${projectDirectory} && rm index.starter.txt`;
+  const createExpressStarterCommand = `cd ${projectDirectory} && rm -rf src/*  && cp index.starter.txt src/index.ts && rm index.starter.txt`;
 
-logTitle('The project was successfully created!');
+  runCommand(gitCloneCommand, '[1/4] Repo cloned successfully!');
+  runCommand(deleteRemoteGitCommand, '[2/4] .git folder removed successfully!');
+  runCommand(installCommand, '[3/4] Dependencies installed successfully!');
+  runCommand(createEnvCommand, '[4/4] .env file created successfully!');
+
+  if (projctType === completeValue) {
+    runCommand(deleteStarterIndexCommand, '[4/4] index.starter.ts file removed successfully!');
+  } else {
+    runCommand(createExpressStarterCommand, '[4/4] express starter setup created successfully!');
+  }
+
+  logTitle('The project was successfully created!');
+})();
